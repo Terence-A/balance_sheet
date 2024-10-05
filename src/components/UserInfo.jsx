e114c0647814dbcfb340d4b02d271cee1ced901a
terenceDev <terence.arseneault@hotmail.com>
2024-10-05 11:45:12 -0300

    added tabs for both coin counts and coin totals under the money tray count

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -3,11 +3,11 @@ const UserInfo = () => {
     <section className="flex justify-evenly text-2xl font-semibold">
       <div className="flex flex-col gap-2">
         <div className="flex gap-2">
-          <h2>Store Name: </h2>
+          <h2 className="font-bold">Store Name: </h2>
           <p className=" border-b border-b-stone-950 w-72 text-center ">6800</p>
         </div>
         <p className="flex gap-2">
-          <label>Till Number:</label>
+          <label className="font-bold">Till Number:</label>
           <input
             type="number"
             className=" border-b border-b-stone-950 w-72 text-center "
@@ -16,7 +16,7 @@ const UserInfo = () => {
       </div>
       <div className="flex flex-col gap-2  ">
         <p className=" flex gap-5 ">
-          <label>Date: </label>
+          <label className="font-bold">Date: </label>
           <input
             className="w-72 text-center border-b border-b-stone-950"
             type="date"
@@ -24,7 +24,7 @@ const UserInfo = () => {
         </p>
 
         <p className="flex gap-2">
-          <label>Name: </label>
+          <label className="font-bold">Name: </label>
           <input
             className="w-72 text-center border-b border-b-stone-950"
             type="text"
