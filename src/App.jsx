ce2030dcab16dcef0422863409044c4584a95e4f
terenceDev <terence.arseneault@hotmail.com>
2024-10-05 12:44:20 -0300

    added print button to print page when completed, also editedited the personal notes to take notes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import CashCounts from "./components/CashCounts";
 import Header from "./components/Header";
 import PersonalNotes from "./components/PersonalNotes";
+import TotalSummary from "./components/TotalSummary";
 import UserInfo from "./components/UserInfo";
-import TotalSummary from "./TotalSummary";
 
 function App() {
   return (
@@ -13,6 +13,11 @@ function App() {
         <CashCounts />
         <TotalSummary />
         <PersonalNotes />
+        <div className="text-center mt-10">
+          <button className="w-32 px-8 py-2 border-2 text-xl font-bold border-stone-900 rounded-lg ">
+            PRINT
+          </button>
+        </div>
       </main>
     </>
   );
